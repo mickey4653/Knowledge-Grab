40e16fc44bfd8d8baf8bd8a61f7a0388ee4a4282
Tyler <tylerbarrett2010@gmail.com>
2021-10-11 21:41:11 -0400

    Created Knowledge-Grab-Specific Debug Message

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end("Hey what's up it's Knowledge Grab :)");
 });
 
 server.listen(port, hostname, () => {
